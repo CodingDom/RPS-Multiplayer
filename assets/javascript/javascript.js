442a122c0eb9ed459fb62e2f902e3c5c4d087fb9
Dom <dominicsmith1021@gmail.com>
2019-01-08 18:53:51 -0500

    Setting up files

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBYPX3g0s3CQKWf8h50SUtg-ww7auC1xO8",
+    authDomain: "rps-multiplayer-bbb91.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-bbb91.firebaseio.com",
+    projectId: "rps-multiplayer-bbb91",
+    storageBucket: "rps-multiplayer-bbb91.appspot.com",
+    messagingSenderId: "199186993993"
+};
+firebase.initializeApp(config);
